49c664fe8b3ac39abe0e8acacf9313b210b2e095
Eric Shans <eric.shans@gmail.com>
2022-03-15 14:50:40 -0400

    updates v2

--- src/pages/Releases.js
+++ src/pages/Releases.js
@@ -6,6 +6,7 @@ function Releases(props) {
       <h2 className="section-header">
         Here are all the releases that are out currently. You can purchase and/or stream them on all services.
       </h2>
+      {/* Loops over component and fetches releases */}
       <Release />
     </>
   )
